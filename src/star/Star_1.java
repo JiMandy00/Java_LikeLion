8eafda49cd4fcd8736c8692d882b0ed273355d8c
DESKTOP-BAFHRBB\c <wkdghk24@gmail.com>
2022-10-17 09:08:21 +0900

    22 10 17
    별찍기 1

---
+++ src/star/Star_1.java
@@ -0,0 +1,10 @@
+package star;
+
+public class Star_1 {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+    }
+}
